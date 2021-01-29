1f33b5d84ea2af79c8a63371730f6c5cdb82e34f
scraper austria <thomas.strehl@at.ibm.com>
2021-01-29 14:44:57 +0100

    Add WeatherMap image for AGES site

--- cwm-rshiny/app.R
+++ cwm-rshiny/app.R
@@ -155,7 +155,7 @@ ui <- fluidPage(
     # Sidebar panel for inputs ----
     sidebarPanel(width=2,
       #p("COVID-19-WeatherMap", align = "left", style="color:darkred"),
-      p("CWM-V0.5.6-20210129"),
+      p("CWM-V0.9.0-20210129"),
 #      tableOutput("secTime"),
       
       fluidRow(
