808acaf567b1e633c784a4896ac83225454aa036
scraper austria <thomas.strehl@at.ibm.com>
2021-02-03 17:42:46 +0100

    Consolidate tooltpis for WeatherMap

--- cwm-rshiny/app.R
+++ cwm-rshiny/app.R
@@ -197,7 +197,7 @@ ui <- fluidPage(
     
     # Sidebar panel for inputs ----
     sidebarPanel(width=2,
-      p("CWM-V0.9.6-20210202"),
+      p("CWM-V0.9.7-20210203"),
       
       fluidRow(
         column(6,
