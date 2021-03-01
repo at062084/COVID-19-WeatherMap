b0d2d329d8b9f8dee142367512a022a53b153f34
scraper austria <thomas.strehl@at.ibm.com>
2021-03-01 18:35:54 +0100

    Overwrite local changes. Preserve local data

--- cwm-rshiny/aws/github.sh
+++ cwm-rshiny/aws/github.sh
@@ -6,5 +6,14 @@ cd $DIR
 mkdir -p "./log" 2>/dev/null
 
 echo "`date +%Y%m%d-%H%M%S` Running $0" | tee -a $LOG
-git pull 2>&1 | tee -a ./log/cwm.bitbucket.log
+
+echo "`date +%Y%m%d-%H%M%S` Fetching master/HEAD. Overwriting code changes. Preserving data" | tee -a $LOG
+mv $DIR/cwm-rshiny/data $DIR/cwm-rshiny/data.org
+
+git fetch 2>&1 | tee -a $LOG
+git reset --hard HEAD 2>&1 | tee -a $LOG
+git merge '@{u}' 2>&1 | tee -a $LOG
+
+mv $DIR/cwm-rshiny/data.org $DIR/cwm-rshiny/data
+
 echo "`date +%Y%m%d-%H%M%S` Finished $0" | tee -a $LOG
