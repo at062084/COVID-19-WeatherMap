39819da57d11e0eee6515e8b6e5f9c36623cebac
scraper austria <thomas.strehl@at.ibm.com>
2021-03-01 18:51:26 +0100

    Bug fix cron base dir and Rscript path

--- cwm-rshiny/aws/cron.sh
+++ cwm-rshiny/aws/cron.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 LOG="./log/cwm.cron.log"
-DIR=/home/shiny/github/COVID-19-WeatherMap
+DIR=/home/shiny/ShinyApps/COVID-19-WeatherMap
 cd $DIR 
 mkdir -p "./log" 2>/dev/null
 
 echo "`date +%Y%m%d-%H%M%S` Running $0" | tee -a $LOG
-Rscript ./cron.R 2>&1 | tee -a $LOG
+/usr/bin/Rscript ./cron.R 2>&1 | tee -a $LOG
 echo "`date +%Y%m%d-%H%M%S` Finished $0" | tee -a $LOG
 
