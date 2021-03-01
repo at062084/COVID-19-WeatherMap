927f000c62adc42de772b85eb6038b57f8a399a2
scraper austria <thomas.strehl@at.ibm.com>
2021-03-01 18:45:14 +0100

    bug fix backup/restore local data

--- cwm-rshiny/aws/github.sh
+++ cwm-rshiny/aws/github.sh
@@ -2,18 +2,22 @@
 
 LOG="./log/cwm.github.log"
 DIR=/home/shiny/github/COVID-19-WeatherMap
+APP=rshiny
+
 cd $DIR 
 mkdir -p "./log" 2>/dev/null
 
 echo "`date +%Y%m%d-%H%M%S` Running $0" | tee -a $LOG
 
 echo "`date +%Y%m%d-%H%M%S` Fetching master/HEAD. Overwriting code changes. Preserving data" | tee -a $LOG
-mv $DIR/cwm-rshiny/data $DIR/cwm-rshiny/data.org
+mkdir -p $DIR/$APP/data.org 2>/dev/null
+mv $DIR/$APP/data/* $DIR/$APP/data.org
 
 git fetch 2>&1 | tee -a $LOG
 git reset --hard HEAD 2>&1 | tee -a $LOG
 git merge '@{u}' 2>&1 | tee -a $LOG
 
-mv $DIR/cwm-rshiny/data.org $DIR/cwm-rshiny/data
+mv $DIR/$APP/data.org/* $DIR/$APP/data
 
 echo "`date +%Y%m%d-%H%M%S` Finished $0" | tee -a $LOG
+
