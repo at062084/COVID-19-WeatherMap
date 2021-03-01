0821538bad70477dcfbd49ace919cdb24704af82
scraper austria <thomas.strehl@at.ibm.com>
2021-03-01 19:01:11 +0100

    suppress eror message

--- cwm-rshiny/aws/github.sh
+++ cwm-rshiny/aws/github.sh
@@ -11,13 +11,13 @@ echo "`date +%Y%m%d-%H%M%S` Running $0" | tee -a $LOG
 
 echo "`date +%Y%m%d-%H%M%S` Fetching master/HEAD. Overwriting code changes. Preserving data" | tee -a $LOG
 mkdir -p $DIR/$APP/data.org 2>/dev/null
-mv $DIR/$APP/data/* $DIR/$APP/data.org
+mv $DIR/$APP/data/* $DIR/$APP/data.org 2>/dev/null
 
 git fetch 2>&1 | tee -a $LOG
 git reset --hard HEAD 2>&1 | tee -a $LOG
 git merge '@{u}' 2>&1 | tee -a $LOG
 
-mv $DIR/$APP/data.org/* $DIR/$APP/data
+mv $DIR/$APP/data.org/* $DIR/$APP/data 2>/dev/null
 
 echo "`date +%Y%m%d-%H%M%S` Finished $0" | tee -a $LOG
 
