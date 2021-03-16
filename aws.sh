bd0acbcae8a4da7d9529c776c332a172b4492e2c
scraper austria <thomas.strehl@at.ibm.com>
2021-03-17 00:04:41 +0100

    update usage

--- aws.sh
+++ aws.sh
@@ -4,7 +4,7 @@ DIR=/home/shiny/ShinyApps/COVID-19-WeatherMap/aws
 
 if [ $# -lt 1 ]
 then
-  echo "usage: $0 <list of shiny server hostnames or IPs> <command to execute>"
+  echo "usage: $0 <command to execute [cron|github]> <list of shiny server hostnames or IPs [rshny-36|rshiny-203]>"
   exit 1
 fi
 
