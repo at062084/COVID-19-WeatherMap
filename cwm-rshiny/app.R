7e6bb7e433d7e8065391dcb9acc4555fa5b31872
scraper austria <thomas.strehl@at.ibm.com>
2021-04-21 10:03:04 +0200

    Update dirs for AWS

--- cwm-rshiny/app.R
+++ cwm-rshiny/app.R
@@ -15,7 +15,7 @@ logMsg <- function(msg, sessionID="_global_") {
 
 hostSystem <- system("hostname", intern=TRUE)
 slackMsg <- function (title, msg, hostName = hostSystem) {
-  url <- as.character(read.csv("../secrets/slack.txt",header=FALSE)[1,1])
+  url <- as.character(read.csv("./secrets/slack.txt",header=FALSE)[1,1])
   body <- list(text = paste(paste0(now()," *",title,"*: "), paste0(hostName,": ",msg)))
   r <- POST(url, content_type_json(), body = body, encode = "json")
   invisible(r)
