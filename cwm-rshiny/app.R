505cc212c9991332b771c14122c0b800d035d539
scraper austria <thomas.strehl@at.ibm.com>
2021-09-01 00:44:55 +0200

    New pages: News, Covid mortability Formulas

--- cwm-rshiny/app.R
+++ cwm-rshiny/app.R
@@ -15,7 +15,7 @@ logMsg <- function(msg, sessionID="_global_") {
 
 hostSystem <- system("hostname", intern=TRUE)
 slackMsg <- function (title, msg, hostName = hostSystem) {
-  url <- as.character(read.csv("../secrets/slack.txt",header=FALSE)[1,1])
+  url <- as.character(read.csv("./secrets/slack.txt",header=FALSE)[1,1])
   body <- list(text = paste(paste0(now()," *",title,"*: "), paste0(hostName,": ",msg)))
   r <- POST(url, content_type_json(), body = body, encode = "json")
   invisible(r)
@@ -309,7 +309,7 @@ ui <- fluidPage(
     
     # Sidebar panel for inputs ----
     sidebarPanel(
-      p("CWM-V1.3.2-20210705"),
+      p("CWM-V1.4.0-20210901"),
       
       fluidRow(
         column(6,
